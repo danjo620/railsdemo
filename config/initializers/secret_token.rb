bb688891818288f1e49829db52ea0d4720b2b8b1
Daniel Jonasson <jonassondaniel@gmail.com>
2011-10-04 19:21:33 +0200

    Clean Rails 3 installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdemo::Application.config.secret_token = '2bdf31bed7696099a53d727e56ab01f2731165ad6180f73c3c0c3d0278ac75c91be8b9244594a81c5cc4dcc18ce3854e673069792266ca28250f499a4dec24e5'
